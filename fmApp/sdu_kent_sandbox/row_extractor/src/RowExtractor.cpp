84752960514a18e638a89aaf02e9487a674cf783
Kent Stark Olsen <kent.stark.olsen@gmail.com>
2013-08-07 07:39:13 +0200

    Added new row extractor package

---
+++ fmApp/sdu_kent_sandbox/row_extractor/src/RowExtractor.cpp
@@ -0,0 +1,17 @@
+/*
+ * RowExtractor.cpp
+ *
+ *  Created on: Aug 3, 2013
+ *      Author: kent
+ */
+
+#include "RowExtractor.h"
+
+RowExtractor::RowExtractor()
+{
+
+}
+
+RowExtractor::~RowExtractor() {
+	// TODO Auto-generated destructor stub
+}
